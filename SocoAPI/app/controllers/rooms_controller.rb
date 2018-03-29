72aa12f4e88fa8ced3d84b76bb1153794044fa8b
SyougoAsada <asapontenhou@gmail.com>
2018-03-29 22:11:26 +0900

    トップページGETクエリ機能完了

--- SocoAPI/app/controllers/rooms_controller.rb
+++ SocoAPI/app/controllers/rooms_controller.rb
@@ -1,21 +1,45 @@
 class RoomsController < ApplicationController
   before_action :take_location
   def index
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-    param = [latitude.to_f, longitude.to_f]
+    param = [@latitude, @longitude]
+    location = Geocoder.search(param)
+    begin
+      comp = location[0].address_components
+      address_comp = comp.map { |adress| adress['long_name'] }[1..-1].reverse
+    rescue NoMethodError
+      @error = 1
+    end
+    if address_comp.length <= 2
+      state = address_comp[0]
+      local = address_comp[1]
+    else
+      state = address_comp[2]
+      local = address_comp[3]
+    end
+    @close_room = Room.within(0.2, origin: [param[0], param[1]])
+                      .select(:name)
+                      .order(created_at: :desc)
+    @state_room = Room.where(state: state)
+                      .select(:name)
+                      .order(created_at: :desc)
+    @local_room = Room.where(local: local)
+                      .select(:name)
+                      .order(created_at: :desc)
 
-    result = Geocoder.search(param)
     render formats: 'json', handlers: 'jbuilder'
   end
 
   def new
     # create rooms
+    # trial -> hodokubo 35.6552625, 139.4109211
   end
 
   private
 
   def take_location
     # lati, longi is saved
+    @error = 0 # error valid
+    @latitude = params[:latitude].to_f
+    @longitude = params[:longitude].to_f
   end
 end
--- SocoAPI/app/views/rooms/index.json.jbuilder
+++ SocoAPI/app/views/rooms/index.json.jbuilder
@@ -1,2 +1,17 @@
-json.state @state
-json.address @address
+if @error == 1
+  json.info do
+    json.error @error
+  end
+else
+  json.info do
+    json.close_room do
+      json.array! @close_room, :name
+    end
+    json.local_room do
+      json.array! @local_room, :name
+    end
+    json.state_room do
+      json.array! @state_room, :name
+    end
+  end
+end
