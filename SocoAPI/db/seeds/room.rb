1d09a4352ea20bdf0c785a11b29ecf7ec0c8a7d8
SyougoAsada <asapontenhou@gmail.com>
2018-04-25 22:14:56 +0900

    seedファイルの実行によりroomのテストデータ作成

--- SocoAPI/db/seeds/room.rb
+++ SocoAPI/db/seeds/room.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+seeds = [
+  {
+    name: "ラウンジ",
+    latitude: -1,
+    longitude: -1
+  },
+  {
+    name: "中央大学",
+    latitude: 35.6413913,
+    longitude: 139.4056186
+  },
+  {
+    name: "中央大学・明星大学駅",
+    latitude: 35.641927,
+    longitude: 139.408568
+  },
+  {
+    name: "立川駅",
+    latitude: 35.697914,
+    longitude: 139.413741
+  },
+  {
+    name: "新宿駅",
+    latitude: 35.691391,
+    longitude: 139.705983,
+  }
+]
+
+seeds.size.times do |i|
+  num = i+1
+  Room.create(
+    id: num,
+    name: seeds[i][:name],
+    latitude: seeds[i][:latitude],
+    longitude: seeds[i][:longitude]
+  )
+end
