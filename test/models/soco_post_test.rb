9355af56aa280a02dbe76d8295ec189cf7604d46
SyougoAsada <asapontenhou@gmail.com>
2018-06-19 00:35:43 +0900

    ローカルにSocoのマイグレーションファイルを別途保存した
    マイグレーションが邪魔でテストが行えなかったため

--- test/models/soco_post_test.rb
+++ test/models/soco_post_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class SocoPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '投稿が成功する想定' do
+    posts = SocoPost.new({
+      contents: '成功想定です',
+      latitude: 1.5,
+      longitude: 1.5
+    })
+    assert posts.save, '保存できませんでした'
+  end
+
+  test '投稿が失敗する想定' do
+    # ここはmodelにvalidationを書いてから記述する
+    posts = SocoPost.new({
+      contents: '失敗する想定です',
+      latitude: 1.5,
+      longitude: 1.5
+    })
+    assert posts.save, '保存できませんでした'
 end
