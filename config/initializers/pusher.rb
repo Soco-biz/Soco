a31caea5fdd98a7ce79fb59a2f8cc7986d598943
SyougoAsada <asapontenhou@gmail.com>
2018-07-15 14:17:38 +0900

    クラスタの変更

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,6 +3,6 @@ require 'pusher'
 Pusher.app_id = ENV['DEV_PUSHER_ID']
 Pusher.key = ENV['DEV_PUSHER_KEY']
 Pusher.secret = ENV['DEV_PUSHER_SECRET']
-Pusher.cluster = 'ap1'
+Pusher.cluster = 'mt1'
 Pusher.logger = Rails.logger
 Pusher.encrypted = true
